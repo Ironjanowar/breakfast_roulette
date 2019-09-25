b434a77d74ba41f590a42b8b5aa566ab5f42da92
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2019-09-26 00:07:58 +0200

    People migration. Docker db and env variables

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,14 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :breakfast_roulette, BreakfastRoulette.Repo,
+  database: System.get_env("PGDATABASE"),
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  hostname: System.get_env("PGHOST"),
+  port: "5432",
+
+config :breakfast_roulette, ecto_repos: [BreakfastRoulette.Repo]
+
 config :ex_gram,
   token: {:system, "BOT_TOKEN"}
